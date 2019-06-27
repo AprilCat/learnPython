bcee98d975ff022796edc616ca6e57d050ca070f
AprilCat <604241857@qq.com>
2019-06-27 23:50:41 +0800

    sigmoid

--- NeuralNetwork/activate_function.py
+++ NeuralNetwork/activate_function.py
@@ -6,9 +6,15 @@ def step_function(x):
     return np.array(x > 0, dtype=int)
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 if __name__ == "__main__":
     x = np.arange(-5, 5, 0.1)
     y = step_function(x)
+    z = sigmoid(x)
     plt.plot(x, y)
+    plt.plot(x, z, linestyle="--")
     plt.ylim(-0.1, 1.1)
     plt.show()
