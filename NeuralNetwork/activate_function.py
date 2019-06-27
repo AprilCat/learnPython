763c430efc38dfd8d55aba30ce751520146955be
AprilCat <604241857@qq.com>
2019-06-27 23:57:46 +0800

    ReLU

--- NeuralNetwork/activate_function.py
+++ NeuralNetwork/activate_function.py
@@ -10,11 +10,17 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def ReLU(x):
+    return np.maximum(0, x)
+
+
 if __name__ == "__main__":
     x = np.arange(-5, 5, 0.1)
-    y = step_function(x)
-    z = sigmoid(x)
-    plt.plot(x, y)
-    plt.plot(x, z, linestyle="--")
+    y1 = step_function(x)
+    y2 = sigmoid(x)
+    y3 = ReLU(x)
+    plt.plot(x, y1)
+    plt.plot(x, y2, linestyle="--", color="red")
+    plt.plot(x, y3)
     plt.ylim(-0.1, 1.1)
     plt.show()
