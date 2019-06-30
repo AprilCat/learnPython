5190fe081c80713ab3bb925112691023768fba51
AprilCat <604241857@qq.com>
2019-06-30 16:44:27 +0800

    softmax

---
+++ NeuralNetwork/softmax.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def softmax(a):
+    max_a = np.max(a)
+    exp_a = np.exp(a - max_a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+if __name__ == "__main__":
+    a = np.array([2000, 2001, 2002])
+    print(softmax(a))
