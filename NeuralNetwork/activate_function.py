f17fa08ffe4e183ba0512047047d00289d1f64e0
AprilCat <604241857@qq.com>
2019-07-12 23:45:18 +0800

    sample of mnist

--- NeuralNetwork/activate_function.py
+++ NeuralNetwork/activate_function.py
@@ -18,6 +18,17 @@ def identical_function(x):
     return x
 
 
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x - np.max(x) # 溢出对策
+    return np.exp(x) / np.sum(np.exp(x))
+
+
 if __name__ == "__main__":
     x = np.arange(-5, 5, 0.1)
     y1 = step_function(x)
--- NeuralNetwork/mnist_test.py
+++ NeuralNetwork/mnist_test.py
@@ -1,6 +1,14 @@
-import sys,os
+import sys, os
+import numpy as np
+from PIL import Image
+from math import sqrt
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
@@ -8,3 +16,7 @@ print(x_train.shape)
 print(t_train.shape)
 print(x_test.shape)
 print(t_test.shape)
+img = x_train[0].reshape(28, 28)
+print(img.shape)
+img_show(img)
+print(t_train[0])
